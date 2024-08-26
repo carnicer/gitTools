0d8a261dc71523f4c06ad427b1af9742e9ad72ae
Marc G Carnicer <m.carnicer@tkhsecurity.com>
2024-08-26 11:12:32 +0200

    fix the lingering ')' in PS1 when not in a git repo

--- ps1no.sh
+++ ps1no.sh
@@ -3,7 +3,9 @@
 
 # PS1='[\u@\h \W]$(__git_ps1 " (\[\e[0;92m\]%s")\[\e[0m\])\$ '
 
-PS1='[\u@\h \[\e[0;94m\]\W\[\e[0m\]]$(__git_ps1 " (\[\e[0;92m\]%s")\[\e[0m\])\$ '
+#PS1='[\u@\h \[\e[0;94m\]\W\[\e[0m\]]$(__git_ps1 " (\[\e[0;92m\]%s")\[\e[0m\])\$ '
+# original albove, below trying to remove the ')' when not on a git clone
+PS1='[\u@\h \[\e[0;94m\]\W\[\e[0m\]]$(__git_ps1 " (\[\e[0;92m\]%s)"\[\e[0m\])\$ '
 
 #PS1='[\u@\h \[\e[0;94m\]\W\[\e[0m\]]$(__git_ps1 " (\[\e[0;32m\]$AE_SANDBOX\[\e[0m\]:\[\e[0;92m\]%s")\[\e[0m\].$PS1TARGET)\$ '
 
